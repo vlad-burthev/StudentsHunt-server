8970238e188ebc74c45989c555de6470104a8d4d
vladburthev <burthev04@gmail.com>
2024-10-03 17:40:52 +0300

    add cors, cookie, validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { NextFunction, Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // удаляет свойства, которые не указаны в DTO
+      forbidNonWhitelisted: true, // выбрасывает ошибку, если присутствуют неразрешенные свойства
+      transform: true, // автоматически преобразует входящие объекты в указанные классы DTO
+    }),
+  );
+
+  app.use(cookieParser());
+  app.use(cors());
+
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    res.cookie('token', '', {
+      httpOnly: true,
+      // secure: true, // Используйте true только на HTTPS
+    });
+    next();
+  });
+
   await app.listen(3000);
 }
 bootstrap();
